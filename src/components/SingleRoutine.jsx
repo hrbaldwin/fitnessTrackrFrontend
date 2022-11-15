aff397c0ab571142cd6bd79f472157fadf4ab09e
Randy Pellegrin <rpell391@gmail.com>
2022-11-15 09:42:09 -0600

    added single routines function

--- src/components/SingleRoutine.jsx
+++ src/components/SingleRoutine.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {fetchingRoutines} from "../api"
 
 const SingleRoutine = () => {
+  const [routines, setRoutines] = useState([]);
+
+    useEffect(() => {
+      const fetchRoutines = async () => {
+        const returnedRoutines = await fetchingRoutines();
+        setRoutines(returnedRoutines);
+      };
+      fetchRoutines();
+    },[])
+
+  
   return (
     <>
       <h2>HI</h2>
